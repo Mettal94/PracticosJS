c820ab1b01db08992f42e212bc2465e060e20877
JNZader <javierzader@hotmail.com>
2024-03-02 18:09:43 -0300

    Update headfoot.js

--- Integrador/js/headfoot.js
+++ Integrador/js/headfoot.js
@@ -9,7 +9,7 @@ let contacto = document.createElement("div");
 inicio.innerHTML = '<a href="index.html">Inicio</a>';
 battle.innerHTML = '<a href="battle.html">Torneo</a>';
 personajes.innerHTML = '<a href="characters.html">Personajes</a>';
-sobreNosotros.innerHTML = '<a href="about.html">Sobre Nosotros</a>';
+sobreNosotros.innerHTML = '<a href="about.html">Quiz</a>';
 contacto.innerHTML = '<a href="contact.html">Contacto</a>';
 
 header.appendChild(inicio);
