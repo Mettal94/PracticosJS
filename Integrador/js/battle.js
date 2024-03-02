506061e7c0d82a0511017aaf1193a91ea3f30457
ApioMan3 <141684285+ApioMan3@users.noreply.github.com>
2024-03-02 01:33:33 -0300

    Battle

--- Integrador/js/battle.js
+++ Integrador/js/battle.js
@@ -18,70 +18,126 @@ var kipiccolo = document.getElementById("kipiccolo").textContent;
 
 
 function iniciarPelea() {
+    desactivarBotones();
     var arena = document.getElementById("arena");
     arena.innerHTML = '';
     let img = document.createElement("img");
     img.src = "img/arena/start.gif";
     arena.appendChild(img);
     var iniciar = document.getElementById("iniciar");
-    iniciar.disabled = true;
-    atkgoku.disabled = false;
-    atkpiccolo.disabled = false;
-    recarga.disabled = false;
-    console.log(vidagoku + " " + kigoku + " " + vidapiccolo + " " + kipiccolo)
+    setTimeout(standby, 1600);
 }
 
 function ataquePiccolo() {
     if (kipiccolo >= 10) {
+        desactivarBotones();
         console.log(kipiccolo);
         var arena = document.getElementById("arena");
         arena.innerHTML = '';
         let img = document.createElement("img");
-        img.src = "img/arena/ataquepiccolo.gif" + '?x=' + Date.now();
+        img.src = "img/arena/ataquepiccolo.gif" + '?x=' + Date.now(); //El date es para recargar el gif
         arena.appendChild(img);
-        var iniciar = document.getElementById("iniciar");
-        recarga.disabled = false;
-
         //resta ki a Piccolo
         kipiccolo = (kipiccolo - 10);
         document.getElementById("kipiccolo").innerText = kipiccolo;
 
         //resta vida a Goku
-        vidagoku = vidagoku - (Math.floor(Math.random() * 25) + 1);
+        vidagoku = vidagoku - (Math.floor(Math.random() * 20) + 5);
         if (vidagoku <= 0) {
             //en caso de bajar a 0, evento de victoria de piccolo
             document.getElementById("vidagoku").innerText = 0;
-            console.log("Piccolo gana");
+            desactivarBotones();
+            setTimeout(piccoloWins, 1900);
         } else {
             //bajar vida normalmente
             document.getElementById("vidagoku").innerText = vidagoku;
+            setTimeout(standby, 2500);
         }
+        
     } else {
-        alert("Piccolo debe recargar.")
+        recargar()
     }
 }
 
 function ataqueGoku() {
-    var arena = document.getElementById("arena");
-    arena.innerHTML = '';
-    let img = document.createElement("img");
-    img.src = "img/arena/ataquegoku.gif" + '?x=' + Date.now();
-    arena.appendChild(img);
-    var iniciar = document.getElementById("iniciar");
-    recarga.disabled = false;
+    if (kigoku >= 10) {
+        desactivarBotones();
+        console.log(kigoku);
+        var arena = document.getElementById("arena");
+        arena.innerHTML = '';
+        let img = document.createElement("img");
+        img.src = "img/arena/ataquegoku.gif" + '?x=' + Date.now(); //El date es para recargar el gif
+        arena.appendChild(img);
+
+        //resta ki a Goku
+        kigoku = (kigoku - 10);
+        document.getElementById("kigoku").innerText = kigoku;
+
+        //resta vida a Piccolo
+        vidapiccolo = vidapiccolo - (Math.floor(Math.random() * 20) + 5);
+        if (vidapiccolo <= 0) {
+            //en caso de bajar a 0, evento de victoria de Goku
+            document.getElementById("vidapiccolo").innerText = 0;
+            desactivarBotones();
+            setTimeout(gokuWins, 1900);
+        } else {
+            //bajar vida normalmente
+            document.getElementById("vidapiccolo").innerText = vidapiccolo;
+            setTimeout(standby, 2700);
+        }
+       
+    } else {
+        recargar()
+    }
 }
 
 function recargar() {
+    desactivarBotones();
     var arena = document.getElementById("arena");
     arena.innerHTML = '';
     let img = document.createElement("img");
     img.src = "img/arena/carga.gif";
     arena.appendChild(img);
     var iniciar = document.getElementById("iniciar");
-    atkgoku.disabled = false;
-    atkpiccolo.disabled = false;
-    recarga.disabled = true;
     setTimeout(standby, 3000);
+    
+    //Generación de recuperación al azar.
+    kigoku = (kigoku + parseInt((Math.floor(Math.random() * 5) + 5)));
+    vidagoku = (vidagoku + parseInt((Math.floor(Math.random() * 30) + 5)));
+    kipiccolo = (kipiccolo + parseInt((Math.floor(Math.random() * 5) + 5)));
+    vidapiccolo = (vidapiccolo + parseInt((Math.floor(Math.random() * 30) + 5)));
+    
+    
+    
+    //Condicionales para que no pasen de 100.
+    if(kigoku>100){
+        document.getElementById("kigoku").innerText = 100;
+        kigoku = 100;   
+    }else{
+    document.getElementById("kigoku").innerText = kigoku;
+    }
+
+    if(kipiccolo>100){
+        document.getElementById("kipiccolo").innerText = 100;
+        kipiccolo = 100;   
+    }else{
+    document.getElementById("kipiccolo").innerText = kipiccolo;
+    }
+
+    if(vidagoku>100){
+        document.getElementById("vidagoku").innerText = 100;
+        vidagoku = 100;   
+    }else{
+    document.getElementById("vidagoku").innerText = vidagoku;
+    }
+
+    if(vidapiccolo>100){
+        document.getElementById("vidapiccolo").innerText = 100;
+        vidapiccolo = 100;   
+    }else{
+    document.getElementById("vidapiccolo").innerText = vidapiccolo;
+    }
+
 }
 
 function standby() {
@@ -90,5 +146,31 @@ function standby() {
     let img = document.createElement("img");
     img.src = "img/arena/standby.png";
     arena.appendChild(img);
+    atkgoku.disabled = false;
+    atkpiccolo.disabled = false;
+    recarga.disabled = false;
+}
+
+function piccoloWins() {
+    var arena = document.getElementById("arena");
+    arena.innerHTML = '';
+    let img = document.createElement("img");
+    img.src = "img/arena/piccolowins.gif";
+    arena.appendChild(img);
+}
+
+function gokuWins() {
+    var arena = document.getElementById("arena");
+    arena.innerHTML = '';
+    let img = document.createElement("img");
+    img.src = "img/arena/gokuwins.gif";
+    arena.appendChild(img);
+}
+
+function desactivarBotones(){
+    iniciar.disabled = true;
+    atkgoku.disabled = true;
+    atkpiccolo.disabled = true;
+    recarga.disabled = true;
 }
 
