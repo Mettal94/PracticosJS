10cfbf6abb257d758d476300466a6280214d9492
Mettal94 <alan.j.schembri@gmail.com>
2024-03-04 18:39:10 -0300

    ultimas correcciones

--- Integrador/js/redireccion.js
+++ Integrador/js/redireccion.js
@@ -1,9 +1,8 @@
-var nombre = localStorage.getItem("contacto");
-var mensaje = document.getElementById("mensaje");
-mensaje.innerHTML = "Gracias por comunicarte con nosotros "+nombre+" !!";
-
 function redireccion(){
     setTimeout(function() {
-        window.location.href = "index.html";
-    }, 2000);
+        window.location.href = "index.html"
+    }, 5000);
 }
+var nombre = localStorage.getItem("contacto");
+var mensaje = document.getElementById("mensaje");
+mensaje.innerHTML = "Gracias por comunicarte con nosotros "+nombre+".";
