734004076782a5539cc3868b351af16bf6a48c13
Mettal94 <alan.j.schembri@gmail.com>
2024-03-04 19:06:18 -0300

    asdad

--- Integrador/js/redireccion.js
+++ Integrador/js/redireccion.js
@@ -1,8 +1,15 @@
-function redireccion(){
     setTimeout(function() {
         window.location.href = "index.html"
     }, 5000);
-}
+
 var nombre = localStorage.getItem("contacto");
 var mensaje = document.getElementById("mensaje");
 mensaje.innerHTML = "Gracias por comunicarte con nosotros "+nombre+".";
+
+var conteo = document.getElementById("conteo");
+
+setInterval(function(){
+    var contador = parseInt(conteo.innerText);
+    contador--;
+    conteo.innerText = contador;
+}, 1000);
