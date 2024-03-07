bc47f0886e3b7d84f4264c6fbe962e088b81aaee
JNZader <javierzader@hotmail.com>
2024-03-07 01:00:40 -0300

    Update contact.js

--- Integrador/js/contact.js
+++ Integrador/js/contact.js
@@ -1,24 +1,32 @@
-function validar(){
+function validar() {
+    // Obtenemos el formulario y los valores de los campos del formulario
     var formulario = document.forms["contacto"];
     var nombre = formulario.nombre.value;
     var apellido = formulario.apellido.value;
     var correo = formulario.correo.value;
     var comment = formulario.comentario.value;
-    var mensaje = document.getElementById("mensaje");
+    var mensaje = document.getElementById("mensaje"); // Elemento para mostrar mensajes de error
 
-    var patronName = /^[^\d\W_]+$/;
-    var patronCorreo = /^\w+@\w+(\.\w{2,4})+$/;
-    
-    try{
-        if(!patronName.test(nombre)||!patronName.test(apellido)) throw "Los datos en el nombre/apellido no son correctos";
+    // Patrones para validar nombre, apellido y correo electrónico
+    var patronName = /^[^\d\W_]+$/; // Solo permite letras y espacios
+    var patronCorreo = /^\w+@\w+(\.\w{2,4})+$/; // Valida formato de correo electrónico
 
-        if(!patronCorreo.test(correo)) throw "Correo no válido";
+    try {
+        // Validamos nombre y apellido utilizando el patrón
+        if (!patronName.test(nombre) || !patronName.test(apellido)) throw "Los datos en el nombre/apellido no son correctos";
 
-        if(!comment) throw "Déjanos un comentario!!"
-    }catch(err){
+        // Validamos el correo electrónico utilizando el patrón
+        if (!patronCorreo.test(correo)) throw "Correo no válido";
+
+        // Validamos que se haya ingresado un comentario
+        if (!comment) throw "Déjanos un comentario!!";
+    } catch (err) {
+        // Si se produce un error, mostramos el mensaje de error y retornamos false para evitar el envío del formulario
         mensaje.innerHTML = err;
         return false;
     }
-    localStorage.setItem("contacto",nombre);
+
+    // Si no hay errores, almacenamos el nombre en el almacenamiento local y retornamos true para enviar el formulario
+    localStorage.setItem("contacto", nombre);
     return true;
 }
